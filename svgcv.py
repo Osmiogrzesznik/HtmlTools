ae90c8706e90bf16448015c8073676122cc0a51f
SourceCowD <boleskok@gmail.com>
2023-03-28 00:41:14 +0100

    Add files via upload

--- svgcv.py
+++ svgcv.py
@@ -4,7 +4,7 @@ from svg_to_gcode.svg_parser import parse_file
 from _compilerWithDynamicFooter import CompilerWithDynamicFooter
 from TrumpfGcode import TrumpfGcode
 from MACROS_CurveEnd import CurveEndMacro
-from Laser1 import Laser1
+from LaserLST import LaserLST
 from Laser2 import Laser2
 from BlankEZpurp import BlankEZpurp
 from svg_to_gcode import TOLERANCES
@@ -13,16 +13,17 @@ import os
 
 parser = TrumpfGcode()
 # parser = CurveEndMacro()
-purpose = BlankEZpurp()
+# purpose = BlankEZpurp()
 # purpose = Laser1()
-purpose = Laser2()
+purpose = LaserLST()
 # purpose = MacroTC_CIRC2() // end of curve will output a circle
 parser.setPurpose(purpose)
 parser.setCutType(100)
 
 # Instantiate a compiler, specifying the custom interface and the speed at which the tool should move.
 # FILENAME = "stencils.svg"
-FILENAME = "stencilsRows.svg"
+# FILENAME = "stencilsRows.svg"
+FILENAME = "gasketTEST.svg"
 filename_wout_ext, file_extension = os.path.splitext(FILENAME)
 NC_compat_filename = filename_wout_ext.upper()
 
